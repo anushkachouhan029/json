361c9b79e5df12c626ca6a75886fe2699237d53c
anushkachouhan029 <anushkachouhan029>
2023-01-20 23:15:45 +0530

    added json inventory

---
+++ UC1_JsonInventoryManagement/Program.cs
@@ -0,0 +1,30 @@
+﻿using Json;
+
+namespace OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Inventory Management ");
+            const string INVENTORY_DATA_PATH = @"D:\RFP BATCH 135\ObjectOrientedPrograms\OOPS\OOPS\InventoryDataManagement\Inventory.json";
+            InventoryManagement inventoryData = new InventoryManagement();
+            Console.WriteLine("Display 1\n");
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Choose your option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        inventoryData.DisplayInventory(INVENTORY_DATA_PATH);
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+    }
+}
